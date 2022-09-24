e82727f137f8ce5733cad6e8f3631561247741dc
gu92maak <100037040+gu92maak@users.noreply.github.com>
2022-09-24 11:11:12 +0200

    Create HelloWorld.kt

---
+++ prototyping/VisualStudioCode/HelloWorld/HelloWorld.kt
@@ -0,0 +1,7 @@
+//install Visual studiocode
+//install Kotlin :  https://www.youtube.com/watch?v=QeVl8fXteI0
+// install java : https://code.visualstudio.com/docs/languages/java
+
+fun main() {
+    print("Hello, World!")
+}
