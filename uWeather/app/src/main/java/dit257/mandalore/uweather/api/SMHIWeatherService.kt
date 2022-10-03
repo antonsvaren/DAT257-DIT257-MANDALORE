7448956ab8d9ae8b8b01abdf0f05d0199258e110
Jamie Brassel <gusbrasja@student.gu.se>
2022-10-03 16:25:13 +0200

    Document API functions

--- uWeather/app/src/main/java/dit257/mandalore/uweather/api/SMHIWeatherService.kt
+++ uWeather/app/src/main/java/dit257/mandalore/uweather/api/SMHIWeatherService.kt
@@ -3,11 +3,9 @@ package dit257.mandalore.uweather.api
 import org.json.JSONObject
 import java.util.concurrent.Future
 
-class SMHIWeatherService :
-    WeatherService(
-        "SMHI",
-        "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2"
-    ) {
+class SMHIWeatherService : WeatherService(
+    "SMHI", "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2"
+) {
     override fun parseResponse(response: String) {
         val timeSeries = JSONObject(response).getJSONArray("timeSeries")
         for (i in 0 until timeSeries.length()) {
--- uWeather/app/src/main/java/dit257/mandalore/uweather/api/WeatherService.kt
+++ uWeather/app/src/main/java/dit257/mandalore/uweather/api/WeatherService.kt
@@ -7,9 +7,21 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
+/**
+ * A generic, unimplemented weather service.
+ *
+ * @property name the human-readable name for the implemented weather service.
+ * @property api the base url for the API sans the request endpoints of the implemented weather
+ * service.
+ */
 abstract class WeatherService(val name: String, private val api: String) {
     companion object {
         val services = sequenceOf(SMHIWeatherService(), MockWeatherService())
+
+        /**
+         * A map of supported cities in Sweden to their coordinates, using the English exonyms. The
+         * coordinates are longitude first, latitude second.
+         */
         val cities = HashMap<String, Pair<Float, Float>>()
 
         init {
@@ -18,6 +30,11 @@ abstract class WeatherService(val name: String, private val api: String) {
             cities["Malmö"] = Pair(13.035833F, 55.60583F)
         }
 
+        /**
+         * Gets a collection of supported city names in no particular order.
+         *
+         * @return the collection of supported city names.
+         */
         fun getCities(): MutableSet<String> {
             return cities.keys
         }
@@ -26,23 +43,55 @@ abstract class WeatherService(val name: String, private val api: String) {
     val responses = arrayListOf<Map<String, Double>>()
     private val executor: ExecutorService = Executors.newSingleThreadExecutor()
 
+    /**
+     * Parses a raw response from the API into [responses].
+     *
+     * @param response the raw response from the weather API.
+     */
     abstract fun parseResponse(response: String)
+
+    /**
+     * Requests the latest weather from the weather API asynchronously at the given location and
+     * calls [parseResponse] with the result.
+     *
+     * @param lon the longitude at which to get the weather.
+     * @param lat the latitude at which to get the weather.
+     * @return the [Future] for and parsing the result, or null
+     */
     abstract fun update(lon: Float, lat: Float): Future<*>?
+
+    /**
+     * Gets the latest temperature information from the API if it exists.
+     *
+     * @return the latest temperature information from the API, or null.
+     */
     abstract fun getCurrentTemperature(): Double?
 
+    /**
+     * Calls [update] after converting the given city name to coordinates.
+     *
+     * @param city the English exonym for the city at which to get the weather.
+     * @return the [Future] for and parsing the result, or null.
+     */
     fun update(city: String): Future<*>? {
         val coords = cities[city]!!
         return update(coords.first, coords.second)
     }
 
+    /**
+     * Sends a request asynchronously to the given endpoint and calls [parseResponse] with the
+     * result.
+     *
+     * @param endpoint the endpoint of the request added to [api].
+     * @return the [Future] for calling the API and parsing the result.
+     */
     fun request(endpoint: String): Future<*> {
         responses.clear()
         return executor.submit {
             try {
-                URL("$api/$endpoint").openStream()
-                    .use {
-                        parseResponse(it.bufferedReader().use(BufferedReader::readText))
-                    }
+                URL("$api/$endpoint").openStream().use {
+                    parseResponse(it.bufferedReader().use(BufferedReader::readText))
+                }
             } catch (e: IOException) {
                 e.printStackTrace()
             }
