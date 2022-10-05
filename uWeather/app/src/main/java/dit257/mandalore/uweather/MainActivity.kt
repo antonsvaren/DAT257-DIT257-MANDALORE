ad37b7caa941b459571ecca4b756c6436e87c5dc
antonsvaren <gussvaanc@student.gu.se>
2022-10-05 14:30:36 +0200

    Created a static navigation bar and implemented the swapping between fragments

--- uWeather/app/src/main/java/dit257/mandalore/uweather/MainActivity.kt
+++ uWeather/app/src/main/java/dit257/mandalore/uweather/MainActivity.kt
@@ -1,6 +1,7 @@
 package dit257.mandalore.uweather
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,25 +10,47 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.*
 import dit257.mandalore.uweather.databinding.ActivityMainBinding
+import dit257.mandalore.uweather.databinding.ContentMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ContentMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ContentMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(FirstFragment())
+
+       binding.bottomNavigationView.setOnItemSelectedListener {
+           when(it.itemId){
+               R.id.weather -> replaceFragment(FirstFragment())
+               R.id.climate -> replaceFragment(SecondFragment())
+               else ->{
+               }
+           }
+           true
+       }
+
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //val navController = findNavController(R.id.nav_host_fragment_content_main)
+        //appBarConfiguration = AppBarConfiguration(navController.graph)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
 
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 /*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,9 +71,12 @@ class MainActivity : AppCompatActivity() {
 
 
  */
+/*
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+*/
+
 }
