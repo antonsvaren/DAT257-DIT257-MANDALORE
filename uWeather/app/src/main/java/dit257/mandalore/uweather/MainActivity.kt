15973cbd47f0bdbf19ede8e40f0b2d42c43ebe7c
irenecanovaas <icanovasvidal2@gmail.com>
2022-10-10 15:54:36 +0200

    overview

--- uWeather/app/src/main/java/dit257/mandalore/uweather/MainActivity.kt
+++ uWeather/app/src/main/java/dit257/mandalore/uweather/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.SearchView
 import androidx.fragment.app.*
 import dit257.mandalore.uweather.databinding.ActivityMainBinding
 import dit257.mandalore.uweather.databinding.ContentMainBinding
@@ -43,7 +45,33 @@ class MainActivity : AppCompatActivity() {
         //val navController = findNavController(R.id.nav_host_fragment_content_main)
         //appBarConfiguration = AppBarConfiguration(navController.graph)
         //setupActionBarWithNavController(navController, appBarConfiguration)
+        val user = arrayOf("Alingsås","Arboga","Arvika","Askersund","Avesta","Boden","Bollnäs","Borgholm","Borlänge","Borås","Djursholm","Eksjö","Enköping","Eskilstuna","Eslöv","Fagersta","Falkenberg","Falköping","Falsterbo","Falun","Filipstad","Flen","Gothenburg","Gränna","Gävle","Hagfors","Halmstad","Haparanda","Hedemora","Helsingborg","Hjo","Hudiksvall","Huskvarna","Härnösand","Hässleholm","Höganäs","Jönköping","Kalmar","Karlshamn","Karlskoga","Karlskrona","Karlstad","Katrineholm","Kiruna","Kramfors","Kristianstad","Kristinehamn","Kumla","Kungsbacka","Kungälv","Köping","Lahol","Landskrona","Lidingö","Lidköping","Lindesberg","Linköping","Ljungby","Ludvika","Luleå","Lund","Lycksele","Lysekil","Malmö","Mariefred","Mariestad","Marstrand","Mjölby","Motala","Nacka","Nora","Norrköping","Norrtälje","Nybro","Nyköping","Nynäshamn","Nässjö","Oskarshamn","Oxelösund","Piteå","Ronneby","Sala","Sandviken","Sigtuna","Simrishamn","Skanör","Skanör med Falsterbo","Skara","Skellefteå","Skänninge","Skövde","Sollefteå","Solna","Stockholm","Strängnäs","Strömstad","Sundbyberg","Sundsvall","Säffle","Säter","Sävsjö","Söderhamn","Söderköping","Södertälje","Sölvesborg","Tidaholm","Torshälla","Tranås","Trelleborg","Trollhättan","Trosa","Uddevalla","Ulricehamn","Umeå","Uppsala","Vadstena","Varberg","Vaxholm","Vetlanda","Vimmerby","Visby","Vänersborg","Värnamo","Västervik","Västerås","Växjö","Ystad","Åmål","Ängelholm","Örebro","Öregrund","Örnsköldsvik","Östersund","Östhammar")
 
+        val userAdapter : ArrayAdapter<String> = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1,
+            user
+        )
+
+        binding.userList.adapter = userAdapter;
+
+        binding.searchView.setOnQueryTextListener(object  : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.searchView.clearFocus()
+                if (user.contains(query)){
+
+                    userAdapter.filter.filter(query)
+
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                userAdapter.filter.filter(newText)
+                return false
+            }
+
+
+        })
     }
 
     private fun replaceFragment(fragment: Fragment){
