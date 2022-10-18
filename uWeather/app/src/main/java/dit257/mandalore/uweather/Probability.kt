b00b0e9d2989377ad9d8d6fc084d574aa885b5f4
Jamie Brassel <gusbrasja@student.gu.se>
2022-10-18 20:12:45 +0200

    Add check for empty or 1 item predictions and fix variance calculation

--- uWeather/app/src/main/java/dit257/mandalore/uweather/Probability.kt
+++ uWeather/app/src/main/java/dit257/mandalore/uweather/Probability.kt
@@ -1,6 +1,8 @@
 package dit257.mandalore.uweather
-import dit257.mandalore.uweather.api.SERVICES
-import kotlin.math.*
+
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 /**
  * We take all the recent temperatures for a specific city and calculating a interval of what the
@@ -9,19 +11,18 @@ import kotlin.math.*
  * choosing.
  */
 fun confidenceInterval(allLastTemps: Sequence<Double>): String {
-    val count = allLastTemps.count()
+    val size = allLastTemps.count()
     val mean = allLastTemps.average()
-    val variance = allLastTemps.sumOf { (it - mean).pow(2) } / (count - 1)
-    val std = sqrt(variance)
-    val tDistributionList = ArrayList<Double>()
-    tDistributionList.addAll(listOf(6.314, 2.92, 2.353, 2.132, 2.015, 1.943, 1.1895, 1.86, 1.833,1.812))
-    val numberOfServices = SERVICES.count()
-    val zAlpha = tDistributionList.get(numberOfServices-2)
-    val confidenceConstant = zAlpha * std / sqrt(count.toDouble())
-    val lower = (mean - confidenceConstant).roundToInt()
-    val upper = (mean + confidenceConstant).roundToInt()
-    if(lower == upper){
-        return "$lower°"
+
+    var lower = mean.roundToInt()
+    var upper = lower
+    if (size > 1) {
+        val variance = allLastTemps.map { (it - mean).pow(2) }.average()
+        val zAlpha = arrayOf(6.314, 2.92)[size - 2]
+        val confidenceConstant = zAlpha * sqrt(variance / size)
+        lower = (mean - confidenceConstant).roundToInt()
+        upper = (mean + confidenceConstant).roundToInt()
     }
-    else return "$lower-$upper°"
+
+    return if (lower == upper) "$lower°" else "$lower-$upper°"
 }
